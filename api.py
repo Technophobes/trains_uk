4d6b8eb9c1f34848c4f35d51f77b3cfe71b333dd
Leah Scott-Zechlin <scottzec@gmail.com>
2020-06-17 10:51:04 +0200

    work on requests

--- api.py
+++ api.py
@@ -2,34 +2,53 @@
 
 # Every request will look like below. Api needs to handle this
 
-{
-	'NLC': 5131,
-	'TLC': 'ABW',
-	'Station Name': 'Abbey Wood',
-	'Region': 'London',
-	'Local Authority': 'Bexley (London)',
-	'Constituency': 'Erith and Thamesmead',
-	'NUTS2 Spatial Unit': 'Outer London - East and North East',
-	'NUTS2 Spatial_Unit Code': 'UKI5',
-	'OS Grid Easting': 547374,
-	'OS Grid Northing': 179035,
-	'Station Facility Owner': 'TfL Rail',
-	'Station Group': nan,
-	'PTE Urban Area Station': nan,
-	'London Travelcard Area': 'London Travelcard Area Station',
-	'Network Rail Region of station': 'Southern',
-	'CRP Line Designation': nan,
-	'Entries & Exits_Full': 882924,
-	'Entries & Exits_Reduced': 1233842,
-	'Entries & Exits_Season': 1652636.0,
-	'1819 Entries & Exits': 3769402,
-	'1718 Entries & Exits': 3124850,
-	'1819 Interchanges': nan,
-	'1819 Entries & Exits - GB rank': 145,
-	'1718 Entries & Exits - GB rank': 176,
-	'Large station change flag': 1.0,
-	'Small station change flag': nan,
-	'% Change': 0.20626654079395812,
-	'Explanation of large change 1819': nan,
-	'Source for explanation of large change 1819': nan
-}
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask_cors import CORS
+from model import dbconnect, Region, Station
+from sqlalchemy import exc
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/region', methods=['POST'])
+def add_region():
+	session = dbconnect()
+	request_dict = request.get_json()
+	try:
+		region_instance = Region()
+		region_instance.region = request_dict["Region"]
+		session.add(region_instance)
+		session.commit()
+		return jsonify(region_instance.id)
+	except exc.IntegrityError:
+		session.rollback()
+		return "already exists", 400
+
+@app.route('/station',  methods=['POST'])
+def add_station():
+	session = dbconnect()
+	request_dict = request.get_json()
+	try:
+		region_instance = session.query(Region).filter(Region.region_name == request_dict["Region"]).one()
+	except:
+		return "Region does not exist, please add it", 400
+
+	try:
+		station = Station()
+		station.region = request_dict["Region"]
+		station.station_name = request_dict["Station Name"]
+		station.total_1819 = request_dict["1819 Entries & Exits"]
+		station.region_id = region_instance.id
+		session.add(station)
+		session.commit()
+		return jsonify(station.id)
+	except exc.IntegrityError:
+		session.rollback()
+		return "already exists", 400
+
+# to get the error message on the url
+if __name__ == '__main__':
+
+	app.run(debug=True)
--- csv_import.py
+++ csv_import.py
@@ -3,6 +3,7 @@
 
 
 import xlrd
+import requests
 from pandas import *
 
 # with xlrd.open_workbook('estimates-of-station-usage-2018-19.xlsx') as workbook:
@@ -11,19 +12,50 @@ from pandas import *
 #         print(target_sheet.row_values(row))
     
 
-xlsx = ExcelFile('short-station-usage.xlsx')
-# for certain sheet: df = xlsx.parse(xlsx.sheet_names[2])
-df = xlsx.parse()
+# xlsx = ExcelFile('short-station-usage.xlsx')
+# # for certain sheet: df = xlsx.parse(xlsx.sheet_names[2])
+# df = xlsx.parse()
 
-def wrangle(row):
+pload_test = {
+	'NLC': 5131,
+	'TLC': 'ABW',
+	'Station Name': 'Abbey Wood',
+	'Region': 'London',
+	# 'Local Authority': 'Bexley (London)',
+	# 'Constituency': 'Erith and Thamesmead',
+	# 'NUTS2 Spatial Unit': 'Outer London - East and North East',
+	# 'NUTS2 Spatial_Unit Code': 'UKI5',
+	# 'OS Grid Easting': 547374,
+	# 'OS Grid Northing': 179035,
+	# 'Station Facility Owner': 'TfL Rail',
+	# 'Station Group': nan,
+	# 'PTE Urban Area Station': nan,
+	# 'London Travelcard Area': 'London Travelcard Area Station',
+	# 'Network Rail Region of station': 'Southern',
+	# 'CRP Line Designation': nan,
+	# 'Entries & Exits_Full': 882924,
+	# 'Entries & Exits_Reduced': 1233842,
+	# 'Entries & Exits_Season': 1652636.0,
+	# '1819 Entries & Exits': 3769402,
+	# '1718 Entries & Exits': 3124850,
+	# '1819 Interchanges': nan,
+	# '1819 Entries & Exits - GB rank': 145,
+	# '1718 Entries & Exits - GB rank': 176,
+	# 'Large station change flag': 1.0,
+	# 'Small station change flag': nan,
+	# '% Change': 0.20626654079395812,
+	# 'Explanation of large change 1819': nan,
+	# 'Source for explanation of large change 1819': nan
+}
+
+# def wrangle(row):
     # requests.post
-    # pload_genus = {"scientific_name": input_dict["genus_scientific"]}
-    # genus_request = requests.post("http://127.0.0.1:5000/genus" , json=pload_genus)
-    # genus_id = genus_request.text
-    # print(genus_id)
-    return row.to_dict()
+region_request = requests.post("http://127.0.0.1:5000/region" , json=pload_test)
+region_id = region_request.text
+print(region_id)
+# return row.to_dict()
 
-request_bodies = df.apply(wrangle, axis=1).tolist()
-print(request_bodies)
+# request_bodies = df.apply(wrangle, axis=1).tolist()
+# print(request_bodies)
 
 # print(df.head())
